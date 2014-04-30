8b9aada94fbf8e124df7ba1b24ea6c2fe59d099f
Alexis Chantreau <achantreau@hotmail.fr>
2014-04-30 14:10:08 +0100

    Website setup script now working properly + Database SQL setup file

--- jSCAPE/database/database.sql
+++ jSCAPE/database/database.sql
@@ -1,3 +1,11 @@
-DROP TABLE xyz
+DROP TABLE Student;
 
-CREATE TABLE xyz
+CREATE TABLE Student (
+    login_name text NOT NULL,
+    first_name text NOT NULL,
+    last_name  text NOT NULL
+);
+
+INSERT INTO Student VALUES ('ac6609', 'Alexis', 'Chantreau');
+INSERT INTO Student VALUES ('jd4510', 'John', 'Doe');
+INSERT INTO Student VALUES ('md1406', 'Matt', 'Damon');
--- setup_website.sh
+++ setup_website.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-cp jSCAPE/dist/* ~/public_html/
+cp jSCAPE/dist/jscape.html ~/public_html/
+cp jSCAPE/dist/jSCAPE.jar ~/public_html/
+cp jSCAPE/dist/jSCAPE.jnlp ~/public_html/
+cp jSCAPE/dist/lib/* ~/public_html/lib/
