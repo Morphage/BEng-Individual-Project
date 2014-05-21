0ea5a0ce40695bcbcdf46863fee159b16cf4ad39
Alexis Chantreau <achantreau@hotmail.fr>
2014-05-21 22:00:22 +0100

    Added to the setup website script

--- setup_website.sh
+++ setup_website.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cp jSCAPE/dist/jscape.html ~/public_html/
-cp jSCAPE/dist/jSCAPE.jar ~/public_html/
-cp jSCAPE/dist/jSCAPE.jnlp ~/public_html/
-cp jSCAPE/dist/lib/* ~/public_html/lib/
+cp jSCAPE\ Client/dist/jSCAPE.html ~/public_html/
+cp jSCAPE\ Client/dist/jSCAPE\ Client.jnlp ~/public_html/
+cp jSCAPE\ Client/dist/jSCAPE_Client.jar ~/public_html/
+cp jSCAPE\ Client/dist/lib/* ~/public_html/lib/
+cp Website/css/* ~/public_html/css/
+cp Website/js/* ~/public_html/js/
+cp Website/html/* ~/public_html/html/
